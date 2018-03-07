07fcc6275845c7d142a588757b4d0ef8e570bbba
mmademann <mjmademann@gmail.com>
2018-03-07 15:30:57 -0800

    use immutability helper in todo reducer

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,17 +1,8 @@
+import update from 'immutability-helper';
+
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      // state as a key/value obj instead of an array
-      // const stateObject = {
-      //   ...state,
-      //   [action.id]:
-      //   {
-      //     id: action.id,
-      //     text: action.text,
-      //     completed: false
-      //   }
-      // }
-      // console.log(stateObject);
       return [
         ...state,
         {
@@ -21,14 +12,34 @@ const todos = (state = [], action) => {
         }
       ]
     case 'TOGGLE_TODO':
-      return state.map(todo =>
-        (todo.id === action.id)
-          ? {...todo, completed: !todo.completed}
-          : todo
-      )
+      return update(state, {
+        [action.id]: {
+          completed: {
+            $set: !state[action.id].completed
+          }
+        }
+      })
+      // // previous return statement using .map
+      // return state.map((todo, index) =>
+      //   (todo.id === action.id)
+      //     ? {...todo, completed: !todo.completed}
+      //     : todo
+      // )
     default:
       return state
   }
 }
 
 export default todos
+
+// state as a key/value obj instead of an array
+// const stateObject = {
+//   ...state,
+//   [action.id]:
+//   {
+//     id: action.id,
+//     text: action.text,
+//     completed: false
+//   }
+// }
+// console.log(stateObject);
